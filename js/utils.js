552600ea920c932887b75267622d0945e41a0cc7
fouck36 <vakhrin.roman@gmail.com>
2020-03-23 16:50:53 +0300

    добавил загрузку с сервера

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,8 @@
 'use strict';
 (function () {
 
+  var DEBOUNCE_INTERVAL = 500;
+
   var getRandomInt = function (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
@@ -66,6 +68,20 @@
     });
   };
 
+  var debounce = function (cb) {
+    var lastTimeout = null;
+
+    return function () {
+      var parameters = arguments;
+      if (lastTimeout) {
+        window.clearTimeout(lastTimeout);
+      }
+      lastTimeout = window.setTimeout(function () {
+        cb.apply(null, parameters);
+      }, DEBOUNCE_INTERVAL);
+    };
+  };
+
 
   window.util = {
     getRandomInt: getRandomInt,
@@ -77,6 +93,7 @@
     deletePins: deletePins,
     defaultSelect: defaultSelect,
     defaultCheckbox: defaultCheckbox,
-    clearInput: clearInput
+    clearInput: clearInput,
+    debounce: debounce
   };
 })();
